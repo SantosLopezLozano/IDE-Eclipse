5f3fea1f7f0d7bdf1b7009b0983ea635606cde9e
Santos López Lozano <santos10502@gmail.com>
2021-02-13 16:01:21 +0100

    Add files via upload
    Primer actualización en local

---
+++ JavaGit/src/HolaMundo.java
@@ -0,0 +1,18 @@
+/**
+ * @author Anfisbena
+ *
+ */
+public class HolaMundo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("hola mundo");
+	}
+	public int caraculo(int a) {
+		return 34;
+	}
+
+}
